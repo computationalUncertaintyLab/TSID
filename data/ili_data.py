fa4c37701c0c544fe2908c98040be6b21b86b7bd
Lela Boermeester <lelab@dyn237152.cc.lehigh.edu>
2026-01-28 14:18:18 -0500

    ili

--- data/ili_data.py
+++ data/ili_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from delphi_epidata import Epidata
 
-res = Epidata.fluview(["nat"], [201501, Epidata.range(201502, 202552)])
+res = Epidata.fluview(["nat","hhs1","hhs2","hhs3","hhs4","hhs5","hhs6","hhs7","hhs8","hhs9","hhs10"], [201501, Epidata.range(201502, 202552)])
 
 d = pd.DataFrame(res["epidata"])
 d.to_csv("ili_data.csv",index=False)
